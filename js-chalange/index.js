8e2c1ab0bb0867e6fb80b2e508207af45c7a31ab
Batrawi <batrawi2000forever@gmail.com>
2023-09-23 03:30:15 +0300

    solve Js challange

--- js-chalange/index.js
+++ js-chalange/index.js
@@ -1,12 +1,28 @@
-// Tap js challenge
-
 const getMissingNumber = (nums) => {
-  let missingNum;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== i)
-    return missingNum = i;
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if (nums[i] === nums[j]) {
+        return `there is a duplicated number : ${nums[i]}`;
+      }
+    }
   }
-  return missingNum
+
+  nums.sort((a, b) => a - b);
+  let missingNum;
+
+  if (nums[nums.length - 1] === n) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== i) {
+        return missingNum = i;
+      }
+    }
+  } else {
+    return "your array doesn't start with 0";
+  }
+
+  return missingNum;
 }
 
-console.log(getMissingNumber([0, 1, 3, 4]));
+console.log(getMissingNumber([1, 2, 3, 5]));
