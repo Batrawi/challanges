28a324667fda98ef577deb99214d599b6f4af530
Batrawi <batrawi2000forever@gmail.com>
2023-09-23 03:51:51 +0300

    add conditions to the code

--- js-chalange/index.js
+++ js-chalange/index.js
@@ -4,25 +4,29 @@ const getMissingNumber = (nums) => {
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
       if (nums[i] === nums[j]) {
-        return `there is a duplicated number : ${nums[i]}`;
+        return `There is a duplicated number: ${nums[i]}`;
       }
     }
   }
 
   nums.sort((a, b) => a - b);
-  let missingNum;
 
-  if (nums[nums.length - 1] === n) {
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] !== i) {
-        return missingNum = i;
-      }
-    }
-  } else {
-    return "your array doesn't start with 0";
+  if (nums[0] !== 0) {
+    return "Your array doesn't start with 0";
   }
 
-  return missingNum;
-}
+  let missingNum;
 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i) {
+      return missingNum = i;
+    }
+  }
+
+  return n;
+}
+console.log(getMissingNumber([3, 0, 1]));
+console.log(getMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+console.log(getMissingNumber([0, 1]));
 console.log(getMissingNumber([1, 2, 3, 5]));
+console.log(getMissingNumber([0, 1, 1, 2, 4]));
